84734ca1dedef9d884e7fa34e30f4613c4782354
Sean Zeliq Urian <seanurian@gmail.com>
2021-12-02 01:55:09 +0700

    add url for prod

--- constant.js
+++ constant.js
@@ -1,5 +1,5 @@
 const CONSTANT = new (function () {
   //   this.baseURL = "http://localhost:3000";
-  this.baseURL = "https://seanurian-be.herokuapp.com/";
+  this.baseURL = "https://seanurian-be.herokuapp.com";
   this.screenShootPath = "/page-screenshot";
 })();
